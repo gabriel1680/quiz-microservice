4fa05664632201afedcbf7a1b3ea8e9b6cb44caf
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2022-08-08 20:51:49 -0300

    feat(quiz): add entity validation

--- src/@core/src/domain/entity/__tests__/quiz.entity.spec.ts
+++ src/@core/src/domain/entity/__tests__/quiz.entity.spec.ts
@@ -1,37 +1,74 @@
 import { idFixture } from "src/@kernel/domain/test/unique-entity-id.fixture";
 import { Answer } from "src/domain/value-object/answer.value-object";
-import { Question } from "../question.entity";
-import { Quiz } from "../quiz.entity";
+import { EntityValidationError, Question } from "../question.entity";
+import { Quiz, QuizProperties } from "../quiz.entity";
 
 describe("Quiz unit tests (Entity)", () => {
+    let validData: QuizProperties;
+    beforeEach(() => {
+        validData = {
+            title: "Quiz sobre animais",
+            questions: [
+                new Question({
+                    title: "some question title",
+                    answers: [
+                        Answer.create({ id: "a", text: "yes" }),
+                        Answer.create({ id: "b", text: "no" }),
+                    ],
+                    correctAnswer: "b",
+                }),
+                new Question({
+                    title: "another question title",
+                    answers: [
+                        Answer.create({ id: "a", text: "yes" }),
+                        Answer.create({ id: "b", text: "no" }),
+                    ],
+                    correctAnswer: "a",
+                }),
+            ],
+        };
+    });
+
     it("should be able to create a Quiz", () => {
-        const questions = [
-            new Question({
-                title: "some question title",
-                answers: [
-                    Answer.create({ id: "a", text: "yes" }),
-                    Answer.create({ id: "b", text: "no" }),
-                ],
-                correctAnswer: "b",
-            }),
-            new Question({
-                title: "another question title",
-                answers: [
-                    Answer.create({ id: "a", text: "yes" }),
-                    Answer.create({ id: "b", text: "no" }),
-                ],
-                correctAnswer: "a",
-            }),
-        ];
-        const quiz = new Quiz(
-            {
-                title: "Quiz sobre animais",
-                questions,
-            },
-            idFixture.generate(),
-        );
+        const quiz = new Quiz(validData, idFixture.generate());
         expect(quiz).toBeInstanceOf(Quiz);
         expect(quiz.title).toBe("Quiz sobre animais");
-        expect(quiz.questions).toHaveLength(questions.length);
+        expect(quiz.questions).toHaveLength(validData.questions.length);
+    });
+
+    it("should be able to throw error if required props are not valid", () => {
+        for (const key of Object.keys(validData)) {
+            const invalidProps = { ...validData };
+            invalidProps[key] = undefined;
+            expect(() => {
+                new Quiz(invalidProps);
+            }).toThrowError(EntityValidationError);
+        }
+    });
+    it("should be able to throw error if the state are not valid", () => {
+        const invalidStates = [
+            {
+                ...validData,
+                questions: [
+                    new Question({
+                        title: "some question title",
+                        answers: [
+                            Answer.create({ id: "a", text: "yes" }),
+                            Answer.create({ id: "b", text: "no" }),
+                        ],
+                        correctAnswer: "b",
+                    }),
+                ],
+            },
+            {
+                ...validData,
+                questions: [{}],
+            },
+        ];
+        for (const state of invalidStates) {
+            expect(() => {
+                new Quiz(state as any);
+            }).toThrowError(EntityValidationError);
+        }
     });
 });
--- src/@core/src/domain/entity/quiz.entity.ts
+++ src/@core/src/domain/entity/quiz.entity.ts
@@ -1,11 +1,36 @@
 import { AggregateRoot } from "src/@kernel/domain/entity/aggregate-root.interface";
 import { Entity, EntityTimestamp } from "src/@kernel/domain/entity/base.entity";
 import { UniqueEntityId } from "src/@kernel/domain/value-object/unique-entity-id.value-object";
-import { Question } from "./question.entity";
+import { EntityValidationError, Question } from "./question.entity";
 
 export class Quiz extends Entity<QuizProperties> implements AggregateRoot {
-    constructor(props: QuizProperties, id: UniqueEntityId) {
+    constructor(props: QuizProperties, id?: UniqueEntityId) {
         super(props, id);
+
+        this.validate();
+    }
+    validate(): void {
+        if (!this.title || typeof this.title !== "string")
+            throw new EntityValidationError(
+                "The field 'title' is required and must be type string",
+            );
+
+        if (!this.questions || !Array.isArray(this.questions))
+            throw new EntityValidationError(
+                "The field 'questions' is required and must be type array",
+            );
+
+        this.questions.forEach(question => {
+            if (!(question instanceof Question))
+                throw new EntityValidationError(
+                    "The field 'questions' is required and must be type Question",
+                );
+        });
+
+        if (this.questions.length < 2)
+            throw new EntityValidationError(
+                "To create a quiz is necessary at least 2 questions",
+            );
     }
     get title(): string {
         return this.props.title;
