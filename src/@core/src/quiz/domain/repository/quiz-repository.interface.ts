75984745db4db368c78521ed610b120e63862e63
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2022-08-14 15:08:41 -0300

    fix(@core/quiz/domain): update repo interface

--- src/@core/src/quiz/domain/repository/quiz-repository.interface.ts
+++ src/@core/src/quiz/domain/repository/quiz-repository.interface.ts
@@ -1,4 +1,8 @@
 import { DefaultRepository, SearchRepository } from "src/@kernel/domain";
 import { Quiz } from "../entity";
+import { SearchQuizParams } from "./quiz.search-params";
+import { QuizSearchResult } from "./quiz.search-result";
 
-export interface QuizRepository extends DefaultRepository<Quiz>, SearchRepository<Quiz> {}
+export interface QuizRepository
+    extends DefaultRepository<Quiz>,
+        SearchRepository<Quiz, string, SearchQuizParams, QuizSearchResult> {}
